f1f9e7e52607c6dc773a945cc88fcdf1b742c839
unyime <unyimeusen01@gmail.com>
2023-01-03 13:58:55 +0100

    Made changes to the UI

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 let myLeads = [];
-const inputBtn = document.getElementById("input-btn");
+const saveLeadBtn = document.getElementById("save-lead-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("leads-container");
-const deleteBtn = document.getElementById("delete-btn");
-const tabBtn = document.getElementById("tab-btn");
+const deleteAllBtn = document.getElementById("delete-all-btn");
+const saveTabBtn = document.getElementById("save-tab-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
-};
+}
 
-tabBtn.addEventListener("click", () => {
+saveTabBtn.addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         myLeads.push(tabs[0].url);
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
         render(myLeads);
-    })
+    });
 });
 
 function render(leads) {
@@ -28,21 +27,25 @@ function render(leads) {
         <li>
             <a href="${leads[i]}" target="_blank">
                 ${leads[i]}
+                <img src="assets/explore.svg" alt="explore icon"/>
             </a>
+            <button title="double-click to delete">
+              <img src="assets/delete.svg" alt="delete icon"/>
+            </button>
         </li>
         `;
     }
 
     ulEl.innerHTML = listItems;
-};
+}
 
-deleteBtn.addEventListener("dblclick", () => {
+deleteAllBtn.addEventListener("dblclick", () => {
     localStorage.removeItem("myLeads");
     myLeads = [];
     render(myLeads);
 });
 
-inputBtn.addEventListener("click", () => {
+saveLeadBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
